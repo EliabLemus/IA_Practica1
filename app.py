04e31ee851da1b3a9979293017129368eb7f5bae
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-12-11 19:28:22 -0600

    Agregado valor fitness

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # app.py
 import csv
 import codecs 
-
 from io import StringIO
-
+import genetic_algoritm
+data = []
 # also importing the request module
 from flask import Flask, render_template, request, jsonify
 
@@ -27,13 +27,25 @@ def form():
         reader = csv.DictReader(fstring, delimiter=',')
         file_up =''
         # Numero de filas
-        row_count = sum(1 for row in reader)
-
-        print('Numero de filas: {}'.format(row_count))
-        for row in reader:
-            file_up += ', '.join(row)
-        
+        list_of_dicts = list(reader)
+        row_count = len(list_of_dicts)
+        individuos = genetic_algoritm.getPopulation(4,1)
+        # individuos=[[0.45,0.2,0.34,0.15]]
+        # print(individuos)
+        for p in individuos:
+            # print(p)
+            for i in list_of_dicts:
+                # file_up += ', '.join(row)
+                data.append(genetic_algoritm.Node(solution=p,realNote=i.get('NOTA FINAL'), projectNotes=[float(i.get('PROYECTO 1')), float(i.get('PROYECTO 2')), float(i.get('PROYECTO 3')), float(i.get('PROYECTO 4'))]))    
+            realNoteArr = []
+            calcNoteArr = []
+            for d in data:
+                realNoteArr.append(float(d.realNote))
+                calcNoteArr.append(float(d.calculatedNote))
+            print('Numero de filas: {}'.format(row_count))
+            genetic_algoritm.fitnessValue(row_count=row_count,realNote=realNoteArr,calculatedNote=calcNoteArr)
         return render_template('form.html')
+    
     
 
         
--- genetic_algoritm.py
+++ genetic_algoritm.py
@@ -18,11 +18,12 @@ def printObject(o):
         print(item, ':', tmp[item])
         
 class Node:
-    def __init__(self, solution = [], fitnessValue = 0, calculatedNote = 0, realNote = 0):
+    def __init__(self, solution = [], fitnessValue = 0, realNote = 0, projectNotes = []):
         self.solution = solution
         self.fitnessValue = fitnessValue
-        self.calculatedNote = calculatedNote
+        self.calculatedNote = calculatedNote(solution, projectNotes)
         self.realNote = realNote
+        self.projectNotes = projectNotes 
 
 #Model: 
 #𝑁𝐶 = 𝑤 𝑃 + 𝑤 𝑃 + 𝑤 𝑃 + 𝑤 𝑃
@@ -30,7 +31,10 @@ class Node:
 #p[1-4] -> Nota proyecto 
 #w[1-4] -> Constantes
 
-solution = [0.45,0.2,0.34,0.15]
+solution = [0.45,0.2,0.34,0.15] # this will be generated 
+# [0]P1 - [1]P2 - [2]P3 - [3]P4 [4]NotaFinal
+entrance=[[55,65,71,61,63.7],[94,20,54,29,44.9],[10,31,16,6,13.35]]
+
 project_score = [[75,50,90,65], [80,95,88,80],[20,55,60,58],[60,28,69,50]]
 real_notes = [71.75,84.65,52.45,53.9]
 
@@ -38,20 +42,27 @@ real_notes = [71.75,84.65,52.45,53.9]
 
 def calculatedNote(*lists):
     return sum(functools.reduce(operator.mul, data) for data in zip(*lists))
-nodes = []
-index = 0
-for k in project_score:
-    calc_note=calculatedNote(solution, k)
-    nodes.append(Node(solution=solution,calculatedNote=calc_note,realNote=real_notes[index]))
-    index+=1
 
-n=len(nodes)
-cuad_error = 0
-for i in nodes:
-    cuad_error += math.pow((i.realNote - i.calculatedNote),2)
-    i.fitnessValue = 1/n * (cuad_error)
+def fitnessValue(row_count=0,realNote = [], calculatedNote = []):
+    substract = [x1 - x2 for (x1, x2) in zip(realNote,calculatedNote)]
+    pow_array = []
+    for s in substract:
+        pow_array.append(s ** 2)
     
-for o in nodes:
-    print('- - -')
-    printObject(o)
+    # print(pow_array)    
+    result = 0
+    print('n: {}'.format(row_count))
+    sum_pow_array = sum(pow_array)
+    print('sum_pow_array:'.format(sum_pow_array))
+    result = 1/row_count * sum(pow_array)
+    print(result)
+    if result != 0:
+        print('Valor fitness: {}'.format(result))
+    else:
+        print('Valor fitness no se pudo calcular')
+    # return 1/n * (realNote - calculateNote )
+    
 
+#foreach solution 
+# - build new node 
+# - build notes 
